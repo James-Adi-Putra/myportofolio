e503f1ee47cf3a60bf34f3e04bb4a310e654835a
James <james.adi@ui.ac.id>
2026-09-13 13:40:20 +0700

    Add unit tests for Education model, view, and template

--- main/tests.py
+++ main/tests.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
-from main.models import Experience
+from main.models import Experience, Education 
 
 class MainTest(TestCase):
     def setUp(self):
+        # Experience
         self.experience = Experience.objects.create(
             title="Mentor PSAF Fasilkom",
             description="Berpartisipasi menjadi mentor pengenalan kampus bagi mahasiswa baru Fasilkom.",
@@ -12,6 +13,14 @@ class MainTest(TestCase):
             thumbnail="mentor.svg",
         )
 
+        #Education
+        self.education = Education.objects.create(
+            institution="Universitas Indonesia",
+            degree="S1 Sistem Informasi",
+            level="university",
+        )
+
+    #Experience Test
     def test_main_url_is_accessible(self):
         response = self.client.get(reverse("main:show_main"))
         self.assertEqual(response.status_code, 200)
@@ -48,5 +57,38 @@ class MainTest(TestCase):
         self.experience.save()
         response = self.client.get(reverse("main:show_experience"))
         self.assertFalse(self.experience.is_ongoing)
+        self.assertContains(response, "Selesai")
+        self.assertNotContains(response, "Sedang berlangsung")
+
+    #Education Test
+    def test_education_url_is_accessible(self):
+        response = self.client.get(reverse("main:show_education"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "education.html")
+
+    def test_education_model(self):
+        self.assertEqual(str(self.education), "Universitas Indonesia")
+        self.assertEqual(self.education.level, "university")
+        self.assertTrue(self.education.is_ongoing)
+
+    def test_education_page(self):
+        response = self.client.get(reverse("main:show_education"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "education.html")
+        self.assertContains(response, self.education.institution)
+        self.assertContains(response, self.education.degree)
+        self.assertContains(response, "Sedang berlangsung")
+        self.assertContains(response, f'href="{reverse("main:show_main")}"')
+
+    def test_empty_education_page(self):
+        Education.objects.all().delete()
+        response = self.client.get(reverse("main:show_education"))
+        self.assertContains(response, "Belum ada riwayat pendidikan yang ditambahkan.")
+
+    def test_completed_education(self):
+        self.education.ended_at = timezone.now()
+        self.education.save()
+        response = self.client.get(reverse("main:show_education"))
+        self.assertFalse(self.education.is_ongoing)
         self.assertContains(response, "Selesai")
         self.assertNotContains(response, "Sedang berlangsung")
